2b2d8d8ad4b1e9c6b3c9b9d059831de699f34db9
Josh Fourie <josh@decoded.ai>
2022-07-27 10:36:57 +1000

    Update main.py with time.sleep
    Signed-off-by: Josh Fourie <josh@decoded.ai>

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 An example for the Decoded.AI preview.
 """
 
-from typing import Any, Callable
+import time
+
+from typing import Any
 
 def get_obs():
     """
@@ -11,27 +13,26 @@ def get_obs():
     # a short story about the world as seen by the AI
     return "The world is a big red square with a big red circle inside it. But you wouldn't know that because they are both red and you are not a computer." 
 
-def get_model() -> Callable[..., Any]:
+def get_model() -> Any:
     """
     Returns a model of the universe.
     """
-    return lambda obs: "Journal entry:" + obs
+    return "21309123123019"
 
 def get_env():
     """
     Returns an environment
     """
-    return lambda: get_obs()
+    return get_obs()
 
-def train_step(model: Callable[..., Any], env: Callable[..., str]):
+def train_step(model: Any, env: Any):
     """
     Trains the model for one step
     """
-    model(
-        env()
-    )
+    time.sleep(1)
+    return model + env
 
-def other_train_loop(model: Callable[..., Any], i: int, env: Any):
+def other_train_loop(model: Any, i: int, env: Any):
     """
     Train the model for i episodes. If the environment is solved/failed before the ith episode, 
     then it is reset and the agent continues until the ith episode.
@@ -39,7 +40,7 @@ def other_train_loop(model: Callable[..., Any], i: int, env: Any):
     for _i in range(i):
         train_step(model, env)
 
-def train_loop(model: Callable[..., Any], i: int, env: Any):
+def train_loop(model: Any, i: int, env: Any):
     """
     Train the model for i episodes. If the environment is solved/failed before the ith episode, 
     then it is reset and the agent continues until the ith episode.
@@ -51,9 +52,8 @@ def main():
     """
     Entrypoint for the program
     """
-    train_loop(get_model(), 2000, get_env())
-    other_train_loop(get_model(), 2000, get_env())
+    train_loop(get_model(), 5, get_env())
+    other_train_loop(get_model(), 5, get_env())
 
 if __name__ == '__main__':
     main()
-    #! decoded export: decoded_logs
